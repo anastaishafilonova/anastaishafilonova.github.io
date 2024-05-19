2c1b33c547ec458298620285519f69aa9314e0d7
anastaishafilonova <filonova.ai@phystech.edu>
2024-05-19 22:35:01 +0300

    feat: Валидировать форму и наладить отправку

--- script.js
+++ script.js
@@ -40,10 +40,12 @@ if (sessionStorage["popupWasClosed"] != 'yes') {
 }
 
 // таймер
-let count = localStorage["curCount"] || 7860*60;  // в секундах
 function startTimer() {
   const countdown = setInterval(function() {
-    count--;
+    let final_time = new Date("May 23, 2024 09:00:00");
+    let cur_time = new Date();
+
+    count = (final_time - cur_time) / 1000;
 
     let day = Math.floor(count / (60*60*24));
     let hour = Math.floor((count % (60*60*24)) / (60*60))
@@ -102,3 +104,100 @@ photos.forEach(function (item) {
         openPhotoPopup(curPopup);
         });
 });
+
+
+//валидация формы
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('edit-form__error_active');
+};
+  
+const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    errorElement.classList.remove('edit-form__error_active');
+    errorElement.textContent = '';
+};
+  
+const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+};
+
+const hasInvalidInput = (inputList) => {
+    return inputList.some((input => {
+      return !input.validity.valid;
+    }));
+};
+const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+      buttonElement.classList.add('edit-form__save-btn_inactive');
+      buttonElement.setAttribute('disabled', true);
+    } else {
+      buttonElement.classList.remove('edit-form__save-btn_inactive');
+      buttonElement.removeAttribute('disabled');
+    }
+};
+  
+const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+    const buttonElement = formElement.querySelector('.form__submit');
+    toggleButtonState(inputList, buttonElement);
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', function () {
+        checkInputValidity(formElement, inputElement);
+        toggleButtonState(inputList, buttonElement);
+      });
+    });
+};
+  
+
+const editForm = document.querySelector('.edit-form');
+editForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    submitForm(editForm);
+});
+
+setEventListeners(editForm);
+const inputTel = editForm.querySelector('#form-tel');
+inputTel.addEventListener('keypress', (evt) => {
+    if (isNaN(Number(evt.key))) {
+        evt.preventDefault();
+    }
+});
+
+
+// отправка формы
+function submitForm(form) {
+    const btn = form.querySelector('.form__submit');
+    btn.value = "Отправляем";
+    btn.classList.add("button-loading");
+    btn.disabled = true;
+    document.body.style.cursor = 'wait';
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: ''
+    }).then((response) => {
+        if (response.ok) {
+          btn.value = "Успешно отправлено";
+          btn.classList.remove("button-loading");
+          btn.classList.add("button-success");
+          document.body.style.cursor = 'default';
+          let inputList = Array.from(form.querySelectorAll('.form__input'));
+          inputList.forEach(input => input.value='');
+          setTimeout(closePopup(form.parentElement.parentElement), 5000)
+        } else {
+          btn.value = "Ошибка!";
+          btn.classList.remove("button-loading");
+          btn.classList.add("button-failure");
+          document.body.style.cursor = 'default';
+        }
+      })
+}
